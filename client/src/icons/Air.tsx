c3759c121bd869caf1c62338649ef49985acbaac
dwlhm <dwiilhamm026@gmail.com>
2022-09-07 22:39:06 +0700

    menambahkan icons

---
+++ client/src/icons/Air.tsx
@@ -0,0 +1,10 @@
+function Air(props: { classes: string }) {
+    return(
+        <svg className={props.classes} width="258" height="122" viewBox="0 0 258 122" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="208" cy="72" r="50" fill="#5AFB9A" className="fill-items"/>
+            <circle cx="115" cy="61" r="61" fill="#5AFB9A" className="fill-items"/>
+            <circle cx="35.5" cy="86.5" r="35.5" fill="#5AFB9A" className="fill-items"/>
+            <rect x="35" y="86" width="173" height="36" fill="#5AFB9A" className="fill-items"/>
+        </svg>
+    )
+}
---
+++ client/src/icons/Location.tsx
@@ -0,0 +1,11 @@
+function Location(props: { classes: string }) {
+    return(
+        <svg className={props.classes} width="110" height="165" viewBox="0 0 110 165" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="55" cy="55" r="55" fill="#5AAEFB"/>
+            <path d="M55 165L7.36863 82.5L102.631 82.5L55 165Z" fill="#5AAEFB"/>
+        </svg>
+
+    )
+}
+
+export default Location
