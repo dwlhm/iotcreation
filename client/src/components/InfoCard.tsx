22e8ec82f1b1d8eed9fb1a4dc0c4d86e07f2b4a6
dwlhm <dwiilhamm026@gmail.com>
2022-09-08 15:47:43 +0700

    perbaikan desain responsive

--- client/src/components/InfoCard.tsx
+++ client/src/components/InfoCard.tsx
@@ -15,21 +15,21 @@ function InfoCard(props: { data: string[]}) {
                 <div className="my-5">
                     <h2 className="text-2xl font-semibold text-white">Partikulat di udara</h2>
                     <div className="my-3">
-                        <div className="bg-white rounded-lg shadow inline-block w-34 p-4 mr-3">
+                        <div className="bg-white rounded-lg shadow inline-block w-34 p-4 mr-3 mb-3">
                             <p className="text-base font-semibold">PM 1.0</p>
                             <div className="font-bold">
                                 <p className="text-3xl inline-block px-3 my-4">{props.data[0]}</p>
                                 <p className="text-xs inline-block">µg/m3</p>
                             </div>
                         </div>
-                        <div className="bg-white rounded-lg shadow inline-block w-34 p-4 mr-3">
+                        <div className="bg-white rounded-lg shadow inline-block w-34 p-4 mr-3 mb-3">
                             <p className="text-base font-semibold">PM 2.5</p>
                             <div className="font-bold">
                                 <p className="text-3xl inline-block px-3 my-4">{props.data[1]}</p>
                                 <p className="text-xs inline-block">µg/m3</p>
                             </div>
                         </div>
-                        <div className="bg-white rounded-lg shadow inline-block w-34 p-4 mr-3">
+                        <div className="bg-white rounded-lg shadow inline-block w-34 p-4 mr-3 mb-3">
                             <p className="text-base font-semibold">PM 10</p>
                             <div className="font-bold">
                                 <p className="text-3xl inline-block px-3 my-4">{props.data[2]}</p>
@@ -41,7 +41,7 @@ function InfoCard(props: { data: string[]}) {
                 <div className="my-10">
                     <h2 className="text-2xl font-semibold text-white">Suhu & Kelembaban Udara</h2>
                     <div className="my-3">
-                        <div className="bg-white rounded-lg shadow inline-block w-96 p-4 mr-3">
+                        <div className="bg-white rounded-lg shadow inline-block w-full sm:w-96 p-4 mr-3">
                             <div className="inline-block border-r-2 border-slate-500 pr-5">
                                 <p className="text-base font-semibold">Suhu</p>
                                 <div className="font-bold">
